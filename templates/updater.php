fd718ebf034b69028ef155317c7359a15bea4749
AndreasMaros <andreas.maros@gebruederheitz.de>
2023-07-06 17:16:12 +0200

    [fix] fix wp nonce

--- templates/updater.php
+++ templates/updater.php
@@ -3,7 +3,7 @@
 </style>
 <div id="app"></div>
 <script>
-    window.ghwpRestNonce = '<?= wp_create_nonce('block-updater'); ?>';
+    window.ghwpRestNonce = '<?= wp_create_nonce('wp_rest'); ?>';
 
     <?= file_get_contents(__DIR__ . '/../templates/assets/block-updater.js') ?>
 </script>
